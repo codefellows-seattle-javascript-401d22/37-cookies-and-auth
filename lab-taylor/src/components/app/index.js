497399c996f02a36f64768848fd7d917e8549825
Taylor Stemple <taylorstemple@gmail.com>
2018-04-04 12:18:12 -0700

    updated app component

--- lab-taylor/src/components/app/index.js
+++ lab-taylor/src/components/app/index.js
@@ -4,12 +4,22 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import appCreateStore from './../../lib/app-store-create.js';
+import * as util from './../../lib/util.js';
+import {tokenSet} from '../../actions/auth-actions.js';
 import Dashboard from './../dashboard';
+import SettingsContainer from './../settings-container';
 
 
 let store = appCreateStore();
 
 class App extends React.Component{
+  componentDidMount() {
+    let token = util.getCookie('X-Sluggram-Token');
+    if(token) {
+      this.props.tokenSet(token);
+    }
+  }
+
   render() {
     return(
       <main className='app-container'>
@@ -23,10 +33,12 @@ class App extends React.Component{
                   <ul>
                     <li><Link to='/welcome/signup'>signup</Link></li>
                     <li><Link to='/welcome/login'>login</Link></li>
+                    <li><Link to='/settings'>settings</Link></li>
                   </ul>
                 </nav>
               </header>
               <Route path='/welcome/:auth' component={Dashboard} />
+              <Route exact path='/settings' component={SettingsContainer} />
             </section>
           </BrowserRouter>
         </Provider>
