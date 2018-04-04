34bb61823b6d47895d27be4dcda1c5ebad0a7957
Taylor Stemple <taylorstemple@gmail.com>
2018-04-04 11:48:18 -0700

    profile component constructor

--- lab-taylor/src/components/profile-form/index.js
+++ lab-taylor/src/components/profile-form/index.js
@@ -3,3 +3,34 @@
 import React from 'react';
 import * as util from './../../lib/util.js';
 
+class ProfileForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = props.profile ? {...props.profile, preview: ''} : { bio: '', avatar: null, preview: ''};
+    
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    let { type, name } = e.target;
+
+    if(name === 'bio') {
+      this.setState({ bio: e.target.value });
+    }
+
+    if(name === 'avatar') {
+      let {files} = e.target;
+      let avatar = files[0];
+      this.setState({avatar});
+      util.imagePreview(avatar)
+      .then(preview => this.setState({preview}))
+      .catch(console.error);
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.onComplete(this.state);
+  }
+}
