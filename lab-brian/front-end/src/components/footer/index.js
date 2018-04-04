b6df4c89886ab161f784da6aca609632aa2b8bfc
Brian Bixby <brianbixby0@gmail.com>
2018-04-04 06:06:28 -0700

    added nav and footer, main.js, index.js are done too

---
+++ lab-brian/front-end/src/components/footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className='footer'>
+        <p> </p>
+        <span><i className="fa fa-github"></i></span>
+        <a href="https://github.com/brianbixby" rel="noreferrer" target="_blank"><i className="fa fa-github"></i> </a>
+        <a href="https://www.linkedin.com/in/brianbixby1/" rel="noreferrer" target="_blank"><i className="fa fa-linkedin-in"></i> </a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
