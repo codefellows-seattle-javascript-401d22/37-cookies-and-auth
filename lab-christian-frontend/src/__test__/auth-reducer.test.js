3c9f6fb55d595405af5881f8be979774a9e0c609
Christian Miller <christian.ax.miller@gmail.com>
2018-04-04 10:44:44 -0700

    added reducer tests

--- lab-christian-frontend/src/__test__/auth-reducer.test.js
+++ lab-christian-frontend/src/__test__/auth-reducer.test.js
@@ -5,4 +5,10 @@ describe('Auth Reducer', function() {
     let result = authReducer(undefined, { type: null });
     expect(result).toEqual(null);
   });
+
+  it('the state should be returned if no action type is provided', () => {
+    let state = { username: 'testusername', email: 'testemail@test.com', password: '123456789' };
+    let result = authReducer(state, { type: null });
+    expect(result).toEqual(state);
+  });
 });
