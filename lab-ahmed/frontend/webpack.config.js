fc6d599db99cd49401dc87b2d1c399a3d6fd1926
Ahmed Ali <Ahmed98148@gmail.com>
2018-04-15 20:11:35 -0700

    fixed webpack babel added folders

--- lab-ahmed/frontend/webpack.config.js
+++ lab-ahmed/frontend/webpack.config.js
@@ -1,86 +1,102 @@
 'use strict';
 
 require('dotenv').config({ path: `${__dirname}/.dev.env` });
-const production = process.env.NODE_ENV === 'production'; //true or false
+const production = process.env.NODE_ENV === 'production';
 
 const {DefinePlugin, EnvironmentPlugin} = require('webpack');
-const HtmlPlugin = require('html-webpack-plugin');
+const HTMLPlugin = require('html-webpack-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
 const UglifyPlugin = require('uglifyjs-webpack-plugin');
 const ExtractPlugin = require('extract-text-webpack-plugin');
 
 let plugins = [
-  new EnvironmentPlugin(['NODE_ENV']), //shorthand for DefinePlugin on process.env keys
+  new EnvironmentPlugin(['NODE_ENV']),
   new ExtractPlugin('bundle-[hash].css'),
-  new HtmlPlugin({ template: `${__dirname}/src/index.html` }),
+  new HTMLPlugin({template: `${__dirname}/src/index.html`}),
   new DefinePlugin({
-    __DEBUG__: JSON.stringify(!production), //webpack constant we can use in our app, this will eventually help us turn our logs on/off with switching this variable out
+    __DEBUG__: JSON.stringify(!production),
     __API_URL__: JSON.stringify(process.env.API_URL),
-  })
-]
+  }),
+];
 
-if (production) {
-  plugins = plugins.concat([new CleanPlugin(), new UglifyPlugin()]);
-}
+if (production)
+  plugins = plugins.concat([ new CleanPlugin(), new UglifyPlugin() ]);
 
 module.exports = {
   plugins,
   entry: `${__dirname}/src/main.js`,
-  devServer: {
-    historyApiFallback: true
+  devServer: { 
+    historyApiFallback: true,
   },
-  devtool: production ? undefined : 'cheap-module-source-map',
+  devtool: production ? undefined : 'cheap-module-eval-source-map',
   output: {
     path: `${__dirname}/build`,
+    filename: 'bundle-[hash].js',
     publicPath: process.env.CDN_URL,
-    filename: 'bundle-[hash].js'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
+        exclude: /node_module/,
+        loader: 'babel-loader',
       },
       {
         test: /\.scss$/,
-        loader: ExtractPlugin.extract(['css-loader','sass-loader'])
+        loader: ExtractPlugin.extract({
+          use: [
+            'css-loader',
+            'resolve-url-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true, 
+                includePaths: [`${__dirname}/src/style`],
+              },
+            },
+          ],
+        }),
       },
       {
-        test: /\.(woff|woff2|ttf|eot|glyph|\.svg)$/,
+        test: /\.icon.svg$/,
+        loader: 'raw-loader',
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot).*/,
+        exclude: /\.icon.svg/,
         use: [
           {
             loader: 'url-loader',
             options: {
               limit: 10000,
-              name: 'font/[name].[ext]'
-            }
-          }
-        ]
+              name: 'font/[name].[hash].[ext]',
+            },
+          },
+        ],
       },
       {
         test: /\.(jpg|jpeg|gif|png|tiff|svg)$/,
-        exclude: /\.glyph.svg/,
+        exclude: /\.icon.svg$/,
         use: [
           {
             loader: 'url-loader',
             options: {
-              limit: 6000,
-              name: 'image/[name].[ext]'
-            }
-          }
-        ]
+              limit: 60000,
+              name: 'image/[name].[ext]',
+            },
+          },
+        ],
       },
       {
-        test: /\.(mp3|aac|aiff|wav|flac|m4a|mp4|ogg)$/,
+        test: /\.(mp3|aac|aiff|wav|flac|m4a|ogg)$/,
         exclude: /\.glyph.svg/,
         use: [
           {
             loader: 'file-loader',
-            options: { name: 'audio/[name].[ext]' }
-          }
-        ]
-      }
-    ]
-  }
-}
+            options: { name: 'audio/[name].[ext]' },
+          },
+        ],
+      },
+    ],
+  },
+};
