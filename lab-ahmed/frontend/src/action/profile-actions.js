669e7c0c96788734464eea93e9f1a5b5d188340e
Ahmed Ali <Ahmed98148@gmail.com>
2018-04-15 20:33:40 -0700

    create auth and actions

---
+++ lab-ahmed/frontend/src/action/profile-actions.js
@@ -0,0 +1,46 @@
+import superagent from 'superagent';
+
+export const userProfileCreate = (profile) => ({
+  type: 'USER_PROFILE_CREATE',
+  payload: profile,
+});
+
+export const userProfileUpdate = (profile) => ({
+  type: 'USER_PROFILE_UPDATE',
+  payload: profile,
+});
+
+
+export const userProfileCreateRequest = (profile) => (dispatch, getState) => {
+  let {auth} = getState();
+  return superagent.post(`${__API_URL__}/profiles`)
+    .set('Authorization', `Bearer ${auth}`)
+    .field('bio', profile.bio)
+    .attach('avatar', profile.avatar)
+    .then(res => {
+      dispatch(userProfileCreate(res.body));
+      return res;
+    });
+};
+
+export const userProfileUpdateRequest = (profile) => (dispatch, getState) => {
+  let {auth} = getState();
+  return superagent.put(`${__API_URL__}/profiles/${profile._id}`)
+    .set('Authorization', `Bearer ${auth}`)
+    .field('bio', profile.bio)
+    .attach('avatar', profile.avatar)
+    .then(res => {
+      dispatch(userProfileCreate(res.body));
+      return res;
+    });
+};
+
+export const userProfileFetchRequest = () => (dispatch, getState) => {
+  let {auth} = getState();
+  return superagent.get(`${__API_URL__}/profiles/me`)
+    .set('Authorization', `Bearer ${auth}`)
+    .then(res => {
+      dispatch(userProfileCreate(res.body));
+      return res;
+    });
+};
--- lab-ahmed/frontend/src/component/app/index.js
+++ lab-ahmed/frontend/src/component/app/index.js
@@ -1,34 +1,45 @@
+import './_app.scss';
 import React from 'react';
-import {Provider} from 'react-redux';
+import {connect} from 'react-redux';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
-import Landing from '../landing';
-import appCreateStore from '../../lib/app-create-store.js';
 
-let store = appCreateStore();
+import Navbar from '../navbar';
+import * as util from '../../lib/util.js';
+import LandingContainer from '../landing-container';
+import DashboardContainer from '../dashboard-container';
+import SettingsContainer from '../settings-container';
+import {tokenSet} from '../../action/auth-actions.js';
+import {userProfileFetchRequest} from '../../action/profile-actions.js';
+
 
 class App extends React.Component {
-  render() {
+  componentDidMount(){
+  }
+
+  render(){
     return (
-      <section className='cfgram'>
-        <Provider store={store}>
-          <BrowserRouter>
-            <section>
-              <header>
-                <h1>frontend cfgram</h1>
-                <nav>
-                  <ul>
-                    <li><Link to='/welcome/signup'>signup</Link></li>
-                    <li><Link to='/welcome/signin'>signin</Link></li>
-                  </ul>
-                </nav>
-              </header>
-              <Route path='/welcome/:auth' component={Landing} />
-            </section>
-          </BrowserRouter>
-        </Provider>
-      </section>
+      <div className='app'>
+        <BrowserRouter>
+          <div>
+            <Route path='*' component={Navbar} />
+            <Route exact path='/welcome/:auth' component={LandingContainer} />
+            <Route exact path='/settings' component={SettingsContainer} />
+            <Route exact path='/dashboard' component={DashboardContainer} />
+            <Route exact path='/' component={DashboardContainer} />
+          </div>
+        </BrowserRouter>
+      </div>
     );
   }
 }
 
-export default App;
+let mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+
+let mapDispatchToProps = (dispatch) => ({
+  tokenSet: (token) => dispatch(tokenSet(token)),
+  userProfileFetch: () => dispatch(userProfileFetchRequest()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
